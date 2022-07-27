a134aae3a93b5aec4c2cff7a8d24ce959f408049
nelsonsandoval <nelson.sandoval@gmail.com>
2022-07-27 14:31:02 -0600

    Part 2 - Dtos Applied

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -1,5 +1,7 @@
 using Catalog.Repositories;
 using Catalog.Entities;
+using Catalog.Dtos;
+using Catalog;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,18 +17,18 @@ namespace Calalog.Controllers{
 
     
         [HttpGet]
-        public IEnumerable<Item> GetItems(){
-            var items = repository.GetItems();
+        public IEnumerable<ItemDto> GetItems(){
+            var items = repository.GetItems().Select( item => item.AsDto() );
             return items;
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItem(Guid id){
+        public ActionResult<ItemDto> GetItem(Guid id){
             var item = repository.GetItem(id);
             if (item is null){
                 return NotFound();
             }
-            return item;
+            return item.AsDto();
         }
 
     }
