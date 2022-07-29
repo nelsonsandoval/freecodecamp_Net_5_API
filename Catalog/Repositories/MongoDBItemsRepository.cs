409237d4b39d6b09ab117960fbe90820e439b805
nelsonsandoval <nelson.sandoval@gmail.com>
2022-07-28 22:15:57 -0600

    Part 4 - Delete method

--- Catalog/Repositories/MongoDBItemsRepository.cs
+++ Catalog/Repositories/MongoDBItemsRepository.cs
@@ -24,7 +24,8 @@ namespace Catalog.Repositories
 
         public void DeleteItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            itemsCollection.DeleteOne(filter);
         }
 
         public Item GetItem(Guid id)
